affebaeeb6a762a318b1ed0c202ff51d542c3b98
eugene <eugeneholenstein@gmail.com>
2023-03-28 17:04:21 +1100

    commit tree 96877e6aba9d983b2908d9c7a3fc300f365e5512

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-
+    word.match /^([aeiou]).+\1$/
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    word.match /^_.ing$/ 
 end
 
 def words_five_letters_long(text)
